8a440326a65a2cb9ad1ef4b23a9f70e02353c59a
Kenny Le <79541722+KennyLe-4@users.noreply.github.com>
2023-01-04 12:10:34 +0000

    initial commit

---
+++ Codecademy/src/SavingsAccount.java
@@ -0,0 +1,51 @@
+//We have made a SavingsAccount class without using any methods beyond main() and the constructor, SavingsAccount().
+//Run the code to see some account behavior happen.
+//Look at the main method! It’s so long! There is so much repeated code! Can you imagine how messy it would look if you needed to make 10 deposits?
+//Throughout this lesson, we will learn how to make methods that would make checking the balance, depositing, and withdrawing all behavior that would take only one line of code.
+
+public class SavingsAccount {
+
+    int balance;
+
+    public SavingsAccount(int initialBalance) {
+        balance = initialBalance;
+    }
+
+    public static void main(String[] args){
+        SavingsAccount savings = new SavingsAccount(2000);
+
+        //Check balance:
+        System.out.println("Hello!");
+        System.out.println("Your balance is "+savings.balance);
+
+        //Withdrawing:
+        int afterWithdraw = savings.balance - 300;
+        savings.balance = afterWithdraw;
+        System.out.println("You just withdrew "+300);
+
+        //Check balance:
+        System.out.println("Hello!");
+        System.out.println("Your balance is "+savings.balance);
+
+        //Deposit:
+        int afterDeposit = savings.balance + 600;
+        savings.balance = afterDeposit;
+        System.out.println("You just deposited "+600);
+
+        //Check balance:
+        System.out.println("Hello!");
+        System.out.println("Your balance is "+savings.balance);
+
+        //Deposit:
+        int afterDeposit2 = savings.balance + 600;
+        savings.balance = afterDeposit2;
+        System.out.println("You just deposited "+600);
+
+        //Check balance:
+        System.out.println("Hello!");
+        System.out.println("Your balance is "+savings.balance);
+
+    }
+}
+
+
