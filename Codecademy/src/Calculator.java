fe00ed41de29c23395e935a36c3ac86267abc0df
Kenny Le <79541722+KennyLe-4@users.noreply.github.com>
2023-01-06 15:36:07 +0000

    Added calculator project

---
+++ Codecademy/src/Calculator.java
@@ -0,0 +1,46 @@
+/*
+This is an Calculator that is used to become a master in math classes
+
+It was created by me, Kenny in 2023. When learning to code in Java.
+*/
+public class Calculator {
+    public Calculator() {
+
+    }
+
+
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int substract(int a, int b){
+        return a - b;
+    }
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    public int divide(int a, int b){
+        return a / b;
+    }
+
+    public int modulo(int a, int b){
+        return a % b;
+    }
+
+    public static void main(String[] args){
+        Calculator myCalculator = new Calculator();
+
+        System.out.println(myCalculator.add(45,11));
+
+        System.out.println(myCalculator.substract(12,6));
+
+        System.out.println(myCalculator.multiply(12,6));
+
+        System.out.println(myCalculator.modulo(10,3));
+
+    }
+
+}
